8611ec6e125d0b1fd62cafbe5be2152f28fefb51
Ekundayo Hannah <ihannahekundayo@gmail.com>
2026-04-17 08:01:20 +0100

    Added 300 java questions

--- src/main/java/com/daniella/repository/QuestionRepository.java
+++ src/main/java/com/daniella/repository/QuestionRepository.java
@@ -3,10 +3,12 @@ package com.daniella.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.daniella.entity.Question;
 import com.daniella.enums.Difficulty;
 
+@Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 	
 	List<Question> findByDifficulty(Difficulty difficulty);
